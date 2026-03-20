48957767c7169a41382e00c8156f18d773514ae0
amitrintzler <amitrintzler@gmail.com>
2026-03-20 18:05:09 +0200

    chore: export ValidationStage from stages package

---
+++ video-pipeline/stages/__init__.py
@@ -0,0 +1,6 @@
+from stages.storyboard import StoryboardStage
+from stages.video import VideoStage
+from stages.stitch import StitchStage
+from stages.validate import ValidationStage, ValidationError
+
+__all__ = ["StoryboardStage", "VideoStage", "StitchStage", "ValidationStage", "ValidationError"]
